922099f5e396c551d4407242f14c88f63ee49b10
ehlee <ehlee@ysmcorp.com>
2019-07-29 13:21:03 +0900

    Udemy - State of the Art Android App Development 샘플 프로젝트 생성

---
+++ UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ykyahwa.udemyartandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
