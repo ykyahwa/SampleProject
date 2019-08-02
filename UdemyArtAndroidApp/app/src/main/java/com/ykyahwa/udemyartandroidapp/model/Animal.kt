bab7edff6b440e0e4f4ee76ef28e41281f8145ba
ehlee <ehlee@ysmcorp.com>
2019-08-02 13:38:10 +0900

    retrofit 추가 작업중

--- UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/model/Animal.kt
+++ UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/model/Animal.kt
@@ -1,3 +1,31 @@
 package com.ykyahwa.udemyartandroidapp.model
 
-data class Animal(val name: String?)
+import com.google.gson.annotations.SerializedName
+
+data class ApiKey(
+    val message: String?,
+    val key: String?
+)
+
+data class Animal(
+    val name: String?,
+    val taxonomy: Taxonomy?,
+    val location: String?,
+    val speed: Speed?,
+    val diet: String,
+    @SerializedName("lifespan")
+    val lifeSpan: String?,
+    @SerializedName("image")
+    val imageUrl: String?
+)
+
+data class Taxonomy(
+    val kingdom: String?,
+    val order: String?,
+    val family: String
+)
+
+data class Speed(
+    val matric: String?,
+    val imperial: String?
+)
