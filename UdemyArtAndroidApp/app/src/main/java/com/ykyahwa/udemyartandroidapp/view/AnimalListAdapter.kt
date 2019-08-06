618fa3a3e4fe94cc3eb71c798bfa6a5f976655df
ehlee <ehlee@ysmcorp.com>
2019-08-06 13:41:31 +0900

    센션6. DetailFragment 구성 Palette 사용

--- UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/view/AnimalListAdapter.kt
+++ UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/view/AnimalListAdapter.kt
@@ -3,6 +3,7 @@ package com.ykyahwa.udemyartandroidapp.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ykyahwa.udemyartandroidapp.R
 import com.ykyahwa.udemyartandroidapp.model.Animal
@@ -30,6 +31,12 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
     override fun onBindViewHolder(holder: ANimalViewHolder, position: Int) {
         holder.view.animalName.text = animalList[position].name
         holder.view.animalImage.loadImage(animalList[position].imageUrl, getProgressDrawable(holder.view.context))
+        holder.view.animalLayout.setOnClickListener {
+            val action = ListFragmentDirections.actionDetail(animalList[position])
+            Navigation.findNavController(holder.view).navigate(action)
+
+
+        }
     }
 
     class ANimalViewHolder(var view: View) : RecyclerView.ViewHolder(view)
--- UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/view/DetailFragment.kt
+++ UdemyArtAndroidApp/app/src/main/java/com/ykyahwa/udemyartandroidapp/view/DetailFragment.kt
@@ -1,19 +1,74 @@
 package com.ykyahwa.udemyartandroidapp.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.ykyahwa.udemyartandroidapp.R
+import com.ykyahwa.udemyartandroidapp.model.Animal
+import com.ykyahwa.udemyartandroidapp.util.getProgressDrawable
+import com.ykyahwa.udemyartandroidapp.util.loadImage
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
+
+    var animal: Animal? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            animal = DetailFragmentArgs.fromBundle(it).animal
+        }
+
+
+        context?.let {
+            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
+        }
+        animalName.text = animal?.name
+        animalLocation.text = animal?.location
+        animalLifespan.text = animal?.lifeSpan
+        animalDiet.text = animal?.diet
+        
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+    }
+
+    private fun setupBackgroundColor(url: String?) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            animalLayout.setBackgroundColor(intColor)
+
+                        }
+                }
+
+            })
+
     }
 }
